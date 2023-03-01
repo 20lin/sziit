0d7623ef59851ea1e1581f5fe8b1bd96fe30423d
“li20021211” <3291976291@qq.com>
2023-03-01 09:25:59 +0800

    “提交项目”

--- aa.java
+++ aa.java
@@ -6,6 +6,8 @@ public class GitTag {
 
        public static void main(String arg[]) {
 
-              System.out.println("Version1.0");
-
+              System.out.println("Version1.0  ");
+              System.out.println("Version2.0  蓝筹股");
+              System.out.println("Version2.0  来敲门");
+              
        }
